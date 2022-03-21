8b4e21ea94e4cd9e9df3522f462137051732d7b0
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2022-03-21 16:22:08 +0100

    even more async stuff

---
+++ fundamentals/async/README.md
@@ -0,0 +1,7 @@
+# Async
+
+## Start
+```npm install
+npm start```
+
+open browser: http://localhost:8080
--- fundamentals/async/promises.js
+++ fundamentals/async/promises.js
@@ -1,10 +1,13 @@
-function asyncFunction() {
+function asyncFunction(success = true, timeout = 1000) {
     return new Promise(function (resolve, reject) {
         const value = 4;
         setTimeout(function () {
-            resolve(value);
-            // reject('Oh no')
-        }, 1000);
+            if (success) {
+                resolve(value);
+            } else {
+                reject('Oh no')
+            }
+        }, timeout);
     })
 }
 
@@ -39,3 +42,25 @@ asyncFunction().then((value) => {
     console.log(value)
     return asyncFunction();
 }).catch(error => console.error(error));
+
+const p1 = asyncFunction(true, 10);
+const p2 = asyncFunction(true, 100);
+const p3 = asyncFunction(true, 1000);
+const p4 = asyncFunction(false, 100);
+
+Promise.all([p1, p2, p3]).then(values => { // ausgeführt, wenn alle Promises resolved sind
+    console.log(values);
+});
+
+Promise.race([p1, p2, p3]).then(values => { // ausgeführt, wenn die erste Promise resolved ist
+    console.log('race', values);
+});
+
+Promise.allSettled([p1, p2, p3, p4]).then(values => { // ausgeführt, wenn alle Promises resolved sind
+    console.log(values);
+});
+
+(async () => {
+    const value = await Promise.allSettled([p1, p2, p3, p4]);
+    console.log('await', value);
+})(); 
