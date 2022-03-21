03889c6abf11211c3a202febf102e2d9a087de24
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2022-03-21 09:49:48 +0100

    initial

---
+++ fundamentals/project/README.md
@@ -0,0 +1,5 @@
+# Documentation
+
+## Start the app
+
+```npm start```
---
+++ fundamentals/project/index.js
@@ -0,0 +1,9 @@
+console.log('Hallo World');
+
+const isOdd = require('is-odd');
+
+console.log(isOdd('1')); //=> true
+console.log(isOdd('3')); //=> true
+
+console.log(isOdd(0)); //=> false
+console.log(isOdd(2)); //=> false
