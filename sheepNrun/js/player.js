a18ff624d3e18502e96fc64671592090f4d99f0c
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2022-03-23 15:22:22 +0100

    Aufgabe 8

--- sheepNrun/js/player.js
+++ sheepNrun/js/player.js
@@ -2,12 +2,14 @@ class Player {
 
     static idle = 'idle';
     static run = 'run';
+    static jump = 'jump';
 
     constructor(context) {
         this.context = context;
         this.images = {
             [Player.idle]: { image: new Image(), steps: 4 },
             [Player.run]: { image: new Image(), steps: 6 },
+            [Player.jump]: { image: new Image(), steps: 8 },
         };
 
         this.currentState = Player.idle;
@@ -34,8 +36,20 @@ class Player {
             }), new Promise((resolve) => {
                 this.images[Player.run].image.src = 'assets/Black_Sheep_Run.png';
                 this.images[Player.run].image.addEventListener('load', resolve);
+            }), new Promise((resolve) => {
+                this.images[Player.jump].image.src = 'assets/Black_Sheep_Jump.png';
+                this.images[Player.jump].image.addEventListener('load', resolve);
             }),
         ]);
+    }
+
+    jump() {
+        if (this.currentState === Player.run) {
+            this.currentState = Player.jump;
+            this.updateEvery = 100;
+            this.y -= 55;
+            this.currentAnimationStep = 0;
+        }
     }
 
     setIsMoving(isMoving) {
@@ -64,6 +78,13 @@ class Player {
     update(timestamp) {
         if (this.shouldUpdate(timestamp)) {
             this.advanceAnimationStep();
+            if (
+                this.currentState === Player.jump &&
+                this.currentAnimationStep === 0
+            ) {
+                this.currentState = Player.run;
+                this.y += 55;
+            }
             this.lastUpdate = timestamp;
         }
     }
