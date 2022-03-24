1aa5a20f357619a3bd89ce1fd4e6041d4f47bacf
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2022-03-24 12:09:34 +0100

    history

---
+++ html5/examples/history/index.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.querySelector('button');
+    button.addEventListener('click', () => {
+        history.pushState('list', '', '/list');
+        console.log('list');
+    });
+});
+
+console.log('foo');
+
+window.onpopstate = (e) => {
+    console.log(e.state);
+    switch (e.state) {
+        case 'list':
+            // display list
+            console.log('list');
+            break;
+        default:
+            // display default
+            console.log('default');
+    }
+}
